20db7936c7892c69e21f2ed38b92e7a96926822b
chiffa <ank@stowers.org>
2014-05-16 17:02:59 -0500

    Added simple cut

--- Philippe_supra.py
+++ Philippe_supra.py
@@ -22,15 +22,17 @@ from scipy import stats
 from mayavi import mlab
 from scipy.spatial import ConvexHull
 
+from tvtk.util.ctf import load_ctfs, rescale_ctfs, save_ctfs
+
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 
 from pylab import get_cmap
 
 ImageRoot = "/home/ank/Documents/projects_files/2014/supra_Philippe/ko fibers"
+scaling_factor = (1.0, 1.0, 3.0)
 
-
-def load_img_dict2():
+def load_img_dict2(load_filter=0.4):
     """
     loads and classifies images from the ImageRoot directory, assuming that there are two channels
 
@@ -47,18 +49,21 @@ def load_img_dict2():
     print 'focal planes detected: %s' % z_stack
     x, y = name_dict[(z_stack,1)].shape
 
-    stack = [np.zeros((x,y,z_stack*3+3)), np.zeros((x,y,z_stack*3+3))]
+    stack = [np.zeros((x,y,z_stack+1)), np.zeros((x,y,z_stack+1))]
 
     for z, plane in name_dict.iteritems():
-        stack[z[1]-1][:,:,z[0]*3] = plane
-        stack[z[1]-1][:,:,z[0]*3+1] = plane
-        stack[z[1]-1][:,:,z[0]*3+2] = plane
+        stack[z[1]-1][:,:,z[0]] = plane
+
+    for sub_stack in stack:
+        flter = sub_stack < sub_stack.max()*load_filter
+        sub_stack[flter] = 0.0
+
 
     return stack[0], stack[1]
 
 
 
-def load_img_dict():
+def load_img_dict(load_filter=0.4):
     """
    loads and classifies images from the ImageRoot directory, assuming that there is only one channel
 
@@ -81,30 +86,39 @@ def load_img_dict():
         stack[:,:,z*3+1] = plane
         stack[:,:,z*3+2] = plane
 
+    flter = stack < stack.max()*load_filter
+    stack[flter] = 0.0
+
     return stack
 
 
-def render_2_channels(chan1, chan2, v_min):
-    """
-    Renders two channels at the same time, one in red and the other in green
+def render_with_cut(chan1, chan2, v_min=0.6, cut = True):
 
-    :param chan1:
-    :param chan2:
-    :param v_min:
-    """
+    s1 = mlab.pipeline.scalar_field(chan1)
+    s1.spacing = scaling_factor
+
+    s2 = mlab.pipeline.scalar_field(chan2)
+    s2.spacing = scaling_factor
+
     red = (1.0, 0.0, 0.0)
     green = (0.0, 1.0, 0.0)
 
-    mlab.pipeline.volume(mlab.pipeline.scalar_field(chan1), color=red, vmin=v_min)
-    mlab.pipeline.volume(mlab.pipeline.scalar_field(chan2), color=green, vmin=v_min)
+    mlab.pipeline.volume(s1, color=red, vmin=v_min, name='RED')
+    mlab.pipeline.volume(s2, color=green, vmin=v_min, name='GREEN')
+
+    if cut:
+        mlab.pipeline.scalar_cut_plane(s1, colormap = 'OrRd')
+        mlab.pipeline.scalar_cut_plane(s2, colormap = 'Greens')
+
 
 
 if __name__ == "__main__":
 
-    stack1, stack2 = load_img_dict2()
-    # stack1 = load_img_dict()
+    stack1, stack2 = load_img_dict2(0.60)
+    # stack1 = load_img_dict(0.0)
 
-    # mlab.pipeline.volume(mlab.pipeline.scalar_field(stack1), vmin=0.40)
-    # mlab.pipeline.volume(mlab.pipeline.scalar_field(stack2), vmin=0.40)
-    render_2_channels(stack1, stack2, 0.50)
+    # mlab.pipeline.volume(stack1, vmin=0.40)
+    # mlab.pipeline.volume(stack2, vmin=0.40)
+    render_with_cut(stack1, stack2, True)
+
     mlab.show()
