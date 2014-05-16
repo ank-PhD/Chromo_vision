87a170a6a25370653a9d416aacab1a60d4f40797
chiffa <ank@stowers.org>
2014-05-16 11:31:51 -0500

    Added two-color render

--- Philippe_supra.py
+++ Philippe_supra.py
@@ -32,7 +32,7 @@ ImageRoot = "/home/ank/Documents/projects_files/2014/supra_Philippe/ko fibers"
 
 def load_img_dict2():
     """
-    loads and classifies images from the ImageRoot directory
+    loads and classifies images from the ImageRoot directory, assuming that there are two channels
 
     :return:
     """
@@ -40,11 +40,11 @@ def load_img_dict2():
     name_dict = {}
     for img in os.listdir(ImageRoot):
         if '.png' in img:
-            print img
+            print '%s image was parsed' % img
             name_dict[tuple(int(element[1:]) for element in img[:-4].split('_')[1:])]=color.rgb2gray(img_as_float(PIL.Image.open(ImageRoot+'/'+img)))
 
     z_stack = max([tpl[0] for tpl in name_dict.keys()])
-    print z_stack
+    print 'focal planes detected: %s' % z_stack
     x, y = name_dict[(z_stack,1)].shape
 
     stack = [np.zeros((x,y,z_stack*3+3)), np.zeros((x,y,z_stack*3+3))]
@@ -60,18 +60,18 @@ def load_img_dict2():
 
 def load_img_dict():
     """
-   loads and classifies images from the ImageRoot directory
+   loads and classifies images from the ImageRoot directory, assuming that there is only one channel
 
     :return:
     """
 
     name_dict = {}
     for img in os.listdir(ImageRoot):
-        print img
+        print '%s image was parsed' % img
         name_dict[int(img[-6:-4])]=color.rgb2gray(img_as_float(PIL.Image.open(ImageRoot+'/'+img)))
 
     z_stack = max(name_dict.keys())
-    print z_stack
+    print 'focal planes detected: %s' % z_stack
     x, y = name_dict[z_stack].shape
 
     stack = np.zeros((x,y,z_stack*3+3))
@@ -84,11 +84,27 @@ def load_img_dict():
     return stack
 
 
+def render_2_channels(chan1, chan2, v_min):
+    """
+    Renders two channels at the same time, one in red and the other in green
+
+    :param chan1:
+    :param chan2:
+    :param v_min:
+    """
+    red = (1.0, 0.0, 0.0)
+    green = (0.0, 1.0, 0.0)
+
+    mlab.pipeline.volume(mlab.pipeline.scalar_field(chan1), color=red, vmin=v_min)
+    mlab.pipeline.volume(mlab.pipeline.scalar_field(chan2), color=green, vmin=v_min)
+
+
 if __name__ == "__main__":
 
     stack1, stack2 = load_img_dict2()
+    # stack1 = load_img_dict()
 
-    print stack1
-
-    mlab.pipeline.volume(mlab.pipeline.scalar_field(stack2), vmin=0.40,)
+    # mlab.pipeline.volume(mlab.pipeline.scalar_field(stack1), vmin=0.40)
+    # mlab.pipeline.volume(mlab.pipeline.scalar_field(stack2), vmin=0.40)
+    render_2_channels(stack1, stack2, 0.50)
     mlab.show()
