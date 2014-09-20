46f1d953609793c508765e105bfb806d48532a8e
chiffa <ank@stowers.org>
2014-09-19 19:34:23 -0500

    Added cell supression. Closes issue #2

--- chr-sep-human.py
+++ chr-sep-human.py
@@ -7,8 +7,10 @@ from time import time
 import mdp
 from itertools import product
 from skimage.segmentation import random_walker, mark_boundaries
-from skimage.morphology import label
-
+from skimage.morphology import label, convex_hull_image
+from skimage.filter import gaussian_filter
+from skimage.measure import perimeter
+from math import pow
 
 # TODO: try inducing a finer separator set
 # TODO: try using the data for the edge detection by assigning a null label to all the edge detectors, so that the data is recovered by diffusion from a skeleton
@@ -82,8 +84,20 @@ def cluster_by_diffusion(data):
 
     return labels2
 
-def watershed(markers):
-    pass
+def cluster_process(labels):
+    rbase = np.zeros(labels.shape)
+    for i in range(1, int(np.max(labels))):
+        base = np.zeros(labels.shape)
+        base[labels==i] = 1
+        li = len(base.nonzero()[0])
+        hull = convex_hull_image(base)
+        lh =len(hull.nonzero()[0])
+        cond = li>4000 and float(lh)/float(li)<1.05
+        print i, li, float(lh)/float(li), cond, pow(perimeter(base), 3.0)/li
+        if cond:
+            rbase = rbase + base
+    return rbase
+
 
 if __name__ == "__main__":
     start = time()
@@ -98,35 +112,45 @@ if __name__ == "__main__":
     sum20[sum20>-0.15] = 0
     sum2  = sum2 + sum20
 
-    # lg1, lg2, _, _ = gabor(bw, 1/64, 8, field=160)
-    #
-    # plt.subplot(2,2,1)
-    # plt.title('Original image')
-    # plt.imshow(bw, cmap='gray', interpolation='nearest')
-    #
-    # plt.subplot(2,2,2)
-    # plt.title('Gabor - line detector')
-    # plt.imshow(lg1, cmap='gray', interpolation='nearest')
-    # plt.colorbar()
-    #
-    # plt.subplot(2,2,3)
-    # plt.title('Gabor - line detector, positive compound only')
-    # # plt.imshow(lg2, cmap='gray', interpolation='nearest')
-    # plt.colorbar()
-    #
-    # plt.subplot(2,2,4)
-    # plt.title('Segmentation - total time %s'%"{0:.2f}".format(time()-start))
-    # # plt.imshow(mark_boundaries(rebw, d_c))
-    # plt.show()
+    bw_blur = gaussian_filter(bw, 10)
+    bwth = np.zeros(bw_blur.shape)
+    bwth[bw_blur>0.3] = 1
+    clsts = label(bwth)*bwth
+    rbase = cluster_process(clsts)[9:,:][:,9:][:-10,:][:,:-10]
+
+    plt.subplot(2,2,1)
+    plt.title('Original image')
+    plt.imshow(bw, cmap='gray', interpolation='nearest')
+
+    plt.subplot(2,2,2)
+    plt.title('Gabor - line detector')
+    plt.imshow(bw_blur, cmap='gray', interpolation='nearest')
+    plt.colorbar()
+
+    plt.subplot(2,2,3)
+    plt.title('Gabor - line detector, positive compound only')
+    plt.imshow(clsts, cmap='spectral', interpolation='nearest')
+    plt.colorbar()
+
+    plt.subplot(2,2,4)
+    plt.title('Segmentation - total time %s'%"{0:.2f}".format(time()-start))
+    plt.imshow(rbase, cmap='gray', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
 
     sum22 = np.copy(sum2)
     sum22[sum2<0] = 0
     d_c = cluster_by_diffusion(sum2)
+    seg_dc = label(d_c, background=0)*(d_c-1)
+    redd = set(seg_dc[rbase>0.01].tolist())
 
+    print redd
+    for i in redd:
+        seg_dc[seg_dc==i] = 1
 
     # align to same shape
     # rebw = bw[4:, :][:,4:]
-    rebw = bw[8:, :][:,8:]
+    rebw = bw[9:,:][:,9:][:-10,:][:,:-10]
 
     plt.subplot(2,2,1)
     plt.title('Original image')
@@ -158,8 +182,8 @@ if __name__ == "__main__":
     plt.colorbar()
 
     plt.subplot(2,2,3)
-    plt.title('Segmentation - total time %s'%"{0:.2f}".format(time()-start))
-    plt.imshow(label(d_c, background=0)*(d_c-1), cmap='spectral', interpolation='nearest')
+    plt.title('Segmentation - total time %s, clusters: %s'%("{0:.2f}".format(time()-start), len(set(seg_dc.flatten().tolist()))) )
+    plt.imshow(seg_dc, cmap='spectral', interpolation='nearest')
     plt.colorbar()
 
     plt.show()
