70ec07221c95ddf57be2c0c9609b1ae10cbffd47
chiffa <ank@andreikucharavy.com>
2016-05-05 20:22:05 -0400

    debugging iteration finished for the yeast.

--- Linhao_masks_logic.py
+++ Linhao_masks_logic.py
@@ -3,7 +3,9 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 from chiffatools.dataviz import smooth_histogram
+from chiffatools.stats import tukey_outliers
 from scipy.ndimage.filters import gaussian_filter
+from scipy import stats
 from collections import defaultdict
 from scipy.stats import pearsonr, ks_2samp
 from csv import writer
@@ -108,14 +110,14 @@ def pre_process(tiff_stack, alpha_clean=5, smoothing_px=1.5, debug=False):
     return stabilized
 
 
-def watershed_segment(base):
+def segment_out_ill_cells(base, debug=False):
     selem = disk(2)
 
     GFP_collector = np.sum(base, axis=0)
     markers = np.zeros(GFP_collector.shape, dtype=np.uint8)
     # watershed segment
-    markers[GFP_collector > np.mean(GFP_collector)*5] = 2
-    markers[GFP_collector < np.mean(GFP_collector)*0.25] = 1
+    markers[GFP_collector > np.mean(GFP_collector)*2] = 2
+    markers[GFP_collector < np.mean(GFP_collector)*0.20] = 1
     labels = random_walker(GFP_collector, markers, beta=10, mode='bf')
     # round up the labels and set the background to 0 from 1.
     labels = closing(labels, selem)
@@ -124,43 +126,107 @@ def watershed_segment(base):
     distance = ndi.distance_transform_edt(labels)
     local_maxi = peak_local_max(distance,
                                 indices=False,  # we want the image mask, not peak position
-                                min_distance=15,  # about half of a bud with our size
-                                threshold_abs=5,  # allows to clear the noise
+                                min_distance=10,  # about half of a bud with our size
+                                threshold_abs=10,  # allows to clear the noise
                                 labels=labels)
     # we fuse the labels that are close together that escaped the min distance in local_maxi
     local_maxi = ndi.convolve(local_maxi, np.ones((5, 5)), mode='constant', cval=0.0)
     # finish the watershed
     markers2 = ndi.label(local_maxi, structure=np.ones((3, 3)))[0]
     labels2 = watershed(-distance, markers2, mask=labels)
+    # there is still the problem with first element not being labeled properly.
 
-    plt.subplot(231)
-    plt.imshow(GFP_collector, interpolation='nearest')
+    # calculating the excessively luminous outliers
+    # => Wrap into an another function
+    segments = []
+    labels3 = np.zeros_like(labels2).astype(np.float64)
+    qualifying_GFP = GFP_collector > np.median(GFP_collector[GFP_collector > 0])
+    for i in range(1, np.max(labels2)+1):
+        current_mask = labels2 == i
+        coll_sel = GFP_collector[np.logical_and(current_mask, qualifying_GFP)]
 
-    plt.subplot(232)
-    plt.imshow(markers, cmap='hot', interpolation='nearest')
+        # if debug:
+        #     print coll_sel.shape
+        #     print coll_sel
+        #     plt.subplot(221)
+        #     plt.imshow(current_mask, cmap='gray', interpolation='nearest')
+        #     plt.subplot(222)
+        #     plt.imshow(qualifying_GFP, cmap='gray', interpolation='nearest')
+        #     plt.subplot(223)
+        #     plt.imshow(np.logical_and(current_mask, qualifying_GFP),
+        #                cmap='gray', interpolation='nearest')
+        #     plt.subplot(224)
+        #     plt.imshow(GFP_collector, cmap='gray', interpolation='nearest')
+        #     plt.show()
 
-    plt.subplot(233)
-    plt.imshow(labels, cmap='gray', interpolation='nearest')
+        GFP_percentile = np.percentile(coll_sel, 50)
 
-    plt.subplot(234)
-    plt.imshow(distance, cmap='gray', interpolation='nearest')
+        GFP_average = np.average(GFP_collector[np.logical_and(current_mask,
+                                                                 GFP_collector>GFP_percentile)])
+        segments.append(GFP_average)
+        labels3[current_mask] = GFP_average
 
-    plt.subplot(235)
-    plt.imshow(markers2, cmap=plt.cm.spectral, interpolation='nearest')
+    argsort = np.argsort(np.array(segments))
+    segments = sorted(segments)
 
-    plt.subplot(236)
-    plt.imshow(labels2, cmap=plt.cm.spectral, interpolation='nearest')
+    support = range(0, len(segments))
+    # requires at least 10 pts to work
+    slope, intercept, _, _, _ = stats.linregress(np.array(support)[1:5],
+                                                 np.array(segments)[1:5])
+    # stderr = np.std(np.array(segments)[1:5])
+    stderr = 0.05/8.
+    predicted = intercept+slope*np.array(support)
+    closure = argsort[np.array(support)[np.array(predicted+stderr*8) < np.array(segments)]]
+    print closure, closure.tolist() != []
+    labels4 = np.zeros_like(labels2).astype(np.uint8)
+    if closure.tolist() != []:
+        print 'enter closure correction'
+        for idx in closure.tolist():
+            labels4[labels2 == idx+1] = 1  # indexing starts from 1, not 0 for the labels
+            print 'updated %s' % (idx+1)
+    # <= Stop the wrap here
 
-    plt.show()
+    if debug:
+        plt.subplot(241)
+        plt.imshow(GFP_collector, interpolation='nearest')
 
-def analyze(name_pattern, w1448, w2561, prefilter=True):
+        plt.subplot(242)
+        plt.imshow(markers, cmap='hot', interpolation='nearest')
 
+        plt.subplot(243)
+        plt.imshow(labels, cmap='gray', interpolation='nearest')
+
+        plt.subplot(244)
+        plt.imshow(labels2, cmap=plt.cm.spectral, interpolation='nearest')
+
+        plt.subplot(245)
+        plt.imshow(labels3, cmap='hot', interpolation='nearest')
+        plt.colorbar()
+
+        plt.subplot(246)
+        plt.plot(segments, 'ko')
+        plt.plot(predicted, 'r')
+        plt.plot(predicted+stderr*8, 'g')  # arbitrary coefficient, but works well
+        plt.plot(predicted-stderr*8, 'g')
+
+        plt.subplot(247)
+        plt.imshow(labels4, cmap='gray', interpolation='nearest')
+
+        plt.subplot(248)
+        plt.imshow(qualifying_GFP)
+
+        plt.show()
+
+    return labels4
+
+
+def analyze(name_pattern, w1448, w2561, prefilter=True, debug=False):
+
+    # GFP-unhealthy cells detection logic
     if prefilter:
-        watershed_segment(w1448)
-
-    # TODO: for yeast, add over-fluorescent cell removing logic on w1448 channel
-    #  A way to do is to project all the non-negative pixel mask onto a single plane
-    # then perform random walker segmentation and use the segmented mask as the reference
+        cancellation_mask = segment_out_ill_cells(w1448, debug)
+        w1448[:, cancellation_mask] = 0
+        w2561[:, cancellation_mask] = 0
 
     # TODO: normalize 2561 channel to span 0-1, ALWAYS, since it is our detection back-bone
     seg0 = [name_pattern]
@@ -187,7 +253,7 @@ def test_yeast():
                 pre_process(current_image)
 
     for replica, (w1448, w2561) in replicas.iteritems():
-        print analyze(replica, w1448, w2561)
+        print analyze(replica, w1448, w2561, debug=True)
         # print replica
         # # reference for the quantities calculated: http://www.ncbi.nlm.nih.gov/pmc/articles/PMC3074624/
         # print 'L2 stats'
@@ -206,10 +272,6 @@ def test_yeast():
         # print '\n'
 
 
-def test_blobs_in_yeast():
-    pass
-
-
 def test_mammalians():
     ImageRoot = "L:\\Users\\linghao\\Data for quantification\\Mammalian\\DM_Splitted channels"
 
@@ -221,12 +283,50 @@ def test_mammalians():
             pre_process(current_image, 30, 0)
 
 
-def mammalian_traversal(path):
+def mammalian_traversal():
     main_root = "L:\\Users\\linghao\\Data for quantification\\Mammalian"
 
 
-def yeast_traversal(path):
+def yeast_traversal():
     main_root = "L:\\Users\\linghao\\Data for quantification\\Yeast"
+    replicas = defaultdict(lambda: [0, 0])
+    results_collector = []
+    sucker_list = []
+
+    for current_location, sub_directories, files in os.walk(main_root):
+        print current_location
+        print '\t', files
+        color = None
+        name_pattern = None
+        if files:
+            for img in files:
+                if '.TIF' in img and '_thumb_' not in img:
+                    img_codename = img.split(' ')[0].split('_')
+
+                    print '%s image was parsed, code: %s' % (img, img_codename)
+
+                    current_image = Image.open(os.path.join(current_location, img))
+                    replicas[img_codename[0]+'-'+img_codename[1]][translator[img_codename[2]]] =\
+                        pre_process(current_image)
+
+                    img_codename = img.split(' ')[0].split('_')
+                    prefix = current_location.split('\\')[4:]
+                    color = translator[img_codename[-1]]
+                    name_pattern = ' - '.join(prefix+img_codename[:-1])
+                    current_image = Image.open(os.path.join(current_location, img))
+                    replicas[name_pattern][color] = pre_process(current_image)
+
+            for name_pattern, (w1448, w2561) in replicas.iteritems():
+                # TODO: normalize 2561 channel to span 0-1, ALWAYS, since it is our detection back-bone
+                print name_pattern
+                try:
+                    results_collector.append(analyze(name_pattern, w1448, w2561,
+                                                     prefilter=True, debug=True))
+                except Exception as my_exception:
+                    print traceback.print_exc(my_exception)
+                    sucker_list.append(name_pattern)
+
+            replicas = defaultdict(lambda: [0, 0])
 
 
 def main_traversal(path):
@@ -268,8 +368,8 @@ def main_traversal(path):
                         prefix = current_location.split('\\')[4:]
                         color = translator[img_codename[-1]]
                         name_pattern = ' - '.join(prefix+img_codename[:-1])
-                        # current_image = Image.open(os.path.join(current_location, img))
-                        # replicas[name_pattern][color] = pre_process_yeast(current_image)
+                        current_image = Image.open(os.path.join(current_location, img))
+                        replicas[name_pattern][color] = pre_process(current_image)
                         print '-', name_pattern, color
 
         for name_pattern, (w1448, w2561) in replicas.iteritems():
@@ -304,4 +404,5 @@ def main_traversal(path):
 if __name__ == "__main__":
     test_yeast()
     # main_traversal(main_root)
-    test_mammalians()
+    # test_mammalians()
+    yeast_traversal()
